bb90b1899f0dd2f3a26c489b830cbb4f89338a56
Todd Sharpe <toddsharpe@outlook.com>
2026-03-14 21:16:03 -0400

    String and runtime types in tokens

--- Src/Blackboard/Blackboard.h
+++ Src/Blackboard/Blackboard.h
@@ -3,8 +3,11 @@
 
 #include "Crc.h"
 #include "TypeCodes.h"
+#include "Types.h"
 #include <functional>
 #include <unordered_map>
+#include <cstring>
+#include <algorithm>
 
 using BlackboardID = uint32_t;
 
@@ -12,26 +15,6 @@ BlackboardID BLACKBOARD_ID(const char* name)
 {
 	return Crc::Calc32(name);
 }
-
-/*
- * Access.
- */
-struct Access
-{
-	constexpr Access(bool read, bool write) :
-		Read(read),
-		Write(write)
-	{
-
-	}
-
-	const bool Read;
-	const bool Write;
-};
-
-static inline constexpr Access ReadOnly(true, false);
-static inline constexpr Access WriteOnly(false, true);
-static inline constexpr Access ReadWrite(true, true);
 
 class BlackboardBuilder;
 class BlackboardView;
@@ -45,8 +28,55 @@ public:
 	friend BlackboardBuilder;
 	friend BlackboardView;
 
+	constexpr Token() :
+		m_id()
+	{
+
+	}
+
+	bool IsValid() const
+	{
+		return m_id != 0;
+	}
+
 private:
 	BlackboardID m_id;
+};
+
+struct InputToken : public Token
+{
+public:
+	constexpr InputToken(const TypeCode type) :
+		Token(),
+		Type(type)
+	{
+
+	}
+	const TypeCode Type;
+};
+
+struct OutputToken : public Token
+{
+public:
+	constexpr OutputToken(const TypeCode type) :
+		Token(),
+		Type(type)
+	{
+
+	}
+	const TypeCode Type;
+};
+
+struct StateToken : public Token
+{
+public:
+	constexpr StateToken(const TypeCode type) :
+		Token(),
+		Type(type)
+	{
+
+	}
+	const TypeCode Type;
 };
 
 template <typename T>
@@ -54,11 +84,13 @@ class Input : public Token
 {
 public:
 	using T_Value = T;
-	constexpr Input() : Token()
+	constexpr Input() :
+		Token()
 	{
 
 	}
-	static constexpr Access Access = ReadOnly;
+	static constexpr bool CanRead = true;
+	static constexpr bool CanWrite = false;
 };
 
 template <typename T>
@@ -66,11 +98,13 @@ class Output : public Token
 {
 public:
 	using T_Value = T;
-	constexpr Output() : Token()
+	constexpr Output() :
+		Token()
 	{
 
 	}
-	static constexpr Access Access = WriteOnly;
+	static constexpr bool CanRead = false;
+	static constexpr bool CanWrite = true;
 };
 
 template <typename T>
@@ -78,11 +112,13 @@ class State : public Token
 {
 public:
 	using T_Value = T;
-	constexpr State() : Token()
+	constexpr State() :
+		Token()
 	{
 
 	}
-	static constexpr Access Access = ReadWrite;
+	static constexpr bool CanRead = true;
+	static constexpr bool CanWrite = true;
 };
 
 /*
@@ -101,14 +137,14 @@ public:
 
 	}
 
-	Value(const T& init) :
+	constexpr Value(const T& init) :
 		m_value(init),
 		m_callback()
 	{
 
 	}
 
-	Value(const T& init, const ChangedCallback& callback) :
+	constexpr Value(const T& init, const ChangedCallback& callback) :
 		m_value(init),
 		m_callback(callback)
 	{
@@ -127,6 +163,11 @@ public:
 			m_value = value;
 	}
 
+	T& Ref()
+	{
+		return m_value;
+	}
+
 private:
 	T m_value;
 	ChangedCallback m_callback;
@@ -139,7 +180,7 @@ template <typename T>
 class BlackboardImpl
 {
 public:
-	BlackboardImpl() :
+	constexpr BlackboardImpl() :
 		m_elements()
 	{
 
@@ -170,13 +211,18 @@ public:
 		return m_elements.at(id).Get();
 	}
 
+	T& Ref(const BlackboardID id)
+	{
+		return m_elements.at(id).Ref();
+	}
+
 	size_t Count() const
 	{
 		return m_elements.size();
 	}
 
 private:
-	std::unordered_map<BlackboardID, typename Value<T>> m_elements;
+	std::unordered_map<BlackboardID, Value<T>> m_elements;
 };
 
 /*
@@ -186,7 +232,7 @@ template <typename... Types>
 class Blackboard
 {
 public:
-	Blackboard() : m_elements()
+	constexpr Blackboard() : m_elements()
 	{
 
 	}
@@ -219,6 +265,12 @@ public:
 	const T& Get(const BlackboardID id) const
 	{
 		return Get<T>().Get(id);
+	}
+
+	template <typename T>
+	T& Value(const BlackboardID id)
+	{
+		return Get<T>().Ref(id);
 	}
 
 private:
@@ -254,7 +306,6 @@ private:
 /*
  * Blackboard Instance.
  */
-using str_t = const char*;
 using BlackboardInst = Blackboard<
 	uint8_t, uint16_t, uint32_t, uint64_t,
 	int8_t, int16_t, int32_t, int64_t,
@@ -267,38 +318,74 @@ using BlackboardInst = Blackboard<
 class BlackboardView
 {
 public:
-	BlackboardView(BlackboardInst& blackboard) :
+	constexpr BlackboardView() :
+		m_blackboard()
+	{
+
+	}
+
+	constexpr BlackboardView(BlackboardInst* blackboard) :
 		m_blackboard(blackboard)
 	{
 
 	}
 
 	template <typename T>
-	const T::T_Value& Get(const T& token) const
+	const typename T::T_Value& Get(const T& token) const
 	{
-		static_assert(T::Access.Read);
+		static_assert(T::CanRead);
 
-		return m_blackboard.Get<typename T::T_Value>(token.m_id);
+		return m_blackboard->Get<typename T::T_Value>(token.m_id);
+	}
+
+	template <typename T_Value>
+	const T_Value& Get(const InputToken& token) const
+	{
+		return m_blackboard->Get<T_Value>(token.m_id);
+	}
+
+	template <typename T_Value>
+	const T_Value& Get(const StateToken& token) const
+	{
+		return m_blackboard->Get<T_Value>(token.m_id);
 	}
 
 	template <typename T>
-	const T::T_Value& operator[](const T& token) const
+	const typename T::T_Value& operator[](const T& token) const
 	{
-		static_assert(T::Access.Read);
+		static_assert(T::CanRead);
 
-		return m_blackboard.Get<typename T::T_Value>(token.m_id);
+		return m_blackboard->Get<typename T::T_Value>(token.m_id);
 	}
 
 	template <typename T>
-	void Set(const T& token, const T::T_Value value) const
+	void Set(const T& token, const typename T::T_Value value) const
 	{
-		static_assert(T::Access.Write);
+		static_assert(T::CanWrite);
 
-		return m_blackboard.Set<typename T::T_Value>(token.m_id, value);
+		return m_blackboard->Set<typename T::T_Value>(token.m_id, value);
+	}
+
+	template <typename T_Value>
+	void Set(const OutputToken& token, const T_Value value) const
+	{
+		return m_blackboard->Set<T_Value>(token.m_id, value);
+	}
+
+	template <typename T_Value>
+	void Set(const StateToken& token, const T_Value value) const
+	{
+		return m_blackboard->Set<T_Value>(token.m_id, value);
+	}
+
+	template <typename T>
+	typename T::T_Value& operator[](const T& token)
+	{
+		return m_blackboard->Value<typename T::T_Value>(token.m_id);
 	}
 
 private:
-	BlackboardInst& m_blackboard;
+	BlackboardInst* m_blackboard;
 };
 
 /*
@@ -306,7 +393,7 @@ private:
  */
 struct BlackboardStore
 {
-	BlackboardStore() : Blackboard()
+	constexpr BlackboardStore() : Blackboard()
 	{
 
 	}
@@ -329,7 +416,7 @@ public:
 	BlackboardBuilder(BlackboardStore& store, const Operation onOperation = nullptr) :
 		m_store(store),
 		m_onOperation(onOperation),
-		m_path("Root")
+		m_path()
 	{
 
 	}
@@ -340,67 +427,178 @@ public:
 		m_onOperation(onOperation),
 		m_path()
 	{
-		memcpy(m_path, path, N);
+		const size_t copy = std::clamp<size_t>(N, 0, MaxPath);
+		memcpy(m_path, path, copy);
+		m_path[MaxPath] = '\0';
+	}
+
+	BlackboardBuilder Root() const
+	{
+		return BlackboardBuilder(m_store, m_onOperation);
 	}
 
 	BlackboardBuilder SubBuilder(const char *name) const
 	{
-		char full[MaxPath] = {};
-		snprintf(full, sizeof(full), "%s.%s", m_path, name);
+		char full[MaxPath + 1] = {};
+		const int ret = strlen(m_path)
+			? snprintf(full, sizeof(full), "%s.%s", m_path, name)
+			: snprintf(full, sizeof(full), "%s", name);
+		full[ret] = '\0';
+		
 		return BlackboardBuilder(m_store, full, m_onOperation);
 	}
 
 	BlackboardView View() const
 	{
-		return BlackboardView(m_store.Blackboard);
+		return BlackboardView(&m_store.Blackboard);
 	}
 
 	template <typename T>
-	bool Add(const char* name, T& token, T::T_Value init = T::T_Value()) const
+	bool Add(const char* name, T& token, typename T::T_Value init = typename T::T_Value()) const
 	{
-		static_assert(T::Access.Write);
-		
-		char full[MaxPath] = {};
-		const int ret = snprintf(full, sizeof(full), "%s.%s", m_path, name);
-		if (ret >= MaxPath)
+		static_assert(T::CanWrite);
+
+		char full[MaxPath + 1] = {};
+		const int ret = strlen(m_path)
+			? snprintf(full, sizeof(full), "%s.%s", m_path, name)
+			: snprintf(full, sizeof(full), "%s", name);
+		if (static_cast<size_t>(ret) >= MaxPath)
 			return false;
+		full[ret] = '\0';
+
+		if (m_onOperation)
+			m_onOperation(Op::Add, full, GetTypeCode<typename T::T_Value>());
 
 		const BlackboardID id = BLACKBOARD_ID(full);
 		if (!m_store.Blackboard.Add<typename T::T_Value>(id, init))
 			return false;
 
-		if (m_onOperation)
-			m_onOperation(Op::Add, full, GetTypeCode<typename T::T_Value>());
-
 		token.m_id = id;
+		return true;
+	}
+
+	bool Add(const char* name, OutputToken& token) const
+	{
+		switch (token.Type)
+		{
+			case TypeCode::u8:
+				return Add<uint8_t>(name, token);
+			case TypeCode::u16:
+				return Add<uint16_t>(name, token);
+			case TypeCode::u32:
+				return Add<uint32_t>(name, token);
+			case TypeCode::u64:
+				return Add<uint64_t>(name, token);
+
+			case TypeCode::i8:
+				return Add<int8_t>(name, token);
+			case TypeCode::i16:
+				return Add<int16_t>(name, token);
+			case TypeCode::i32:
+				return Add<int32_t>(name, token);
+			case TypeCode::i64:
+				return Add<int64_t>(name, token);
+
+			case TypeCode::f32:
+				return Add<float>(name, token);
+			case TypeCode::f64:
+				return Add<double>(name, token);
+
+			case TypeCode::str:
+				return Add<str_t>(name, token);
+
+			default:
+				ASSERT(false);
+				return false;
+		}
+	}
+
+	template <typename T>
+	bool Add(const char* name, OutputToken& token) const
+	{
+		Output<T> wrapped;
+		if (!Add(name, wrapped))
+			return false;
+
+		token.m_id = wrapped.m_id;
 		return true;
 	}
 
 	template <typename T>
 	bool Get(const char* name, T& token) const
 	{
-		static_assert(T::Access.Read);
-		
-		char full[MaxPath] = {};
-		const int ret = snprintf(full, sizeof(full), "%s.%s", m_path, name);
-		if (ret >= MaxPath)
+		static_assert(T::CanRead);
+
+		char full[MaxPath + 1] = {};
+		const int ret = strlen(m_path)
+			? snprintf(full, sizeof(full), "%s.%s", m_path, name)
+			: snprintf(full, sizeof(full), "%s", name);
+		if (static_cast<size_t>(ret) >= MaxPath)
 			return false;
+		full[ret] = '\0';
+
+		if (m_onOperation)
+			m_onOperation(Op::Get, full, GetTypeCode<typename T::T_Value>());
 
 		const BlackboardID id = BLACKBOARD_ID(full);
 		if (!m_store.Blackboard.Contains<typename T::T_Value>(id))
 			return false;
 
-		if (m_onOperation)
-			m_onOperation(Op::Get, full, GetTypeCode<typename T::T_Value>());
-
 		token.m_id = id;
 		return true;
 	}
 
+	bool Get(const char* name, InputToken& token) const
+	{
+		switch (token.Type)
+		{
+			case TypeCode::u8:
+				return Get<uint8_t>(name, token);
+			case TypeCode::u16:
+				return Get<uint16_t>(name, token);
+			case TypeCode::u32:
+				return Get<uint32_t>(name, token);
+			case TypeCode::u64:
+				return Get<uint64_t>(name, token);
+
+			case TypeCode::i8:
+				return Get<int8_t>(name, token);
+			case TypeCode::i16:
+				return Get<int16_t>(name, token);
+			case TypeCode::i32:
+				return Get<int32_t>(name, token);
+			case TypeCode::i64:
+				return Get<int64_t>(name, token);
+
+			case TypeCode::f32:
+				return Get<float>(name, token);
+			case TypeCode::f64:
+				return Get<double>(name, token);
+				
+			case TypeCode::str:
+				return Get<str_t>(name, token);
+
+			default:
+				ASSERT(false);
+				return false;
+		}
+	}
+
+	template <typename T>
+	bool Get(const char* name, InputToken& token) const
+	{
+		Input<T> wrapped;
+		if (!Get(name, wrapped))
+			return false;
+
+		token.m_id = wrapped.m_id;
+		return true;
+	}
+
 private:
-	static constexpr size_t MaxPath = 128;
+	static constexpr size_t MaxPath = 64;
 
 	BlackboardStore& m_store;
 	Operation m_onOperation;
-	char m_path[MaxPath];
+	char m_path[MaxPath + 1];
 };
--- Src/Blackboard/TypeCodes.h
+++ Src/Blackboard/TypeCodes.h
@@ -1,23 +1,12 @@
 #pragma once
 
+#include "Assert.h"
+#include "Types.h"
 #include <type_traits>
+#include <cstdint>
+#include <cstring>
 
-constexpr char TypeCodeNames[][4] =
-{
-	"u8",
-	"u16",
-	"u32",
-	"u64",
-	"i8",
-	"i16",
-	"i32",
-	"i64",
-	"f32",
-	"f64",
-	"str"
-};
-
-enum class TypeCode
+enum class TypeCode : uint8_t
 {
 	u8,
 	u16,
@@ -32,37 +21,93 @@ enum class TypeCode
 	str
 };
 
+const char TypeCodeNames[][4] = 
+{
+	"u8",
+	"u16",
+	"u32",
+	"u64",
+	"i8",
+	"i16",
+	"i32",
+	"i64",
+	"f32",
+	"f64",
+	"str"
+};
+
 template <typename T>
 constexpr TypeCode GetTypeCode()
 {
-	if constexpr (std::is_same<T, uint8_t>::value)
+	if constexpr (std::is_same<uint8_t, T>::value)
 		return TypeCode::u8;
-	else if constexpr (std::is_same<T, uint16_t>::value)
+	else if constexpr (std::is_same<uint16_t, T>::value)
 		return TypeCode::u16;
-	else if constexpr (std::is_same<T, uint32_t>::value)
+	else if constexpr (std::is_same<uint32_t, T>::value)
 		return TypeCode::u32;
-	else if constexpr (std::is_same<T, uint64_t>::value)
+	else if constexpr (std::is_same<uint64_t, T>::value)
 		return TypeCode::u64;
-	else if constexpr (std::is_same<T, int8_t>::value)
-		return TypeCode::iu8;
-	else if constexpr (std::is_same<T, int16_t>::value)
+
+	else if constexpr (std::is_same<int8_t, T>::value)
+		return TypeCode::i8;
+	else if constexpr (std::is_same<int16_t, T>::value)
 		return TypeCode::i16;
-	else if constexpr (std::is_same<T, int32_t>::value)
+	else if constexpr (std::is_same<int32_t, T>::value)
 		return TypeCode::i32;
-	else if constexpr (std::is_same<T, int64_t>::value)
+	else if constexpr (std::is_same<int64_t, T>::value)
 		return TypeCode::i64;
-	else if constexpr (std::is_same<T, float>::value)
+
+	else if constexpr (std::is_same<float, T>::value)
 		return TypeCode::f32;
-	else if constexpr (std::is_same<T, double>::value)
+	else if constexpr (std::is_same<double, T>::value)
 		return TypeCode::f64;
-	else if constexpr (std::is_same<T, const char*>::value)
+
+	else if constexpr (std::is_same<str_t, T>::value)
 		return TypeCode::str;
+
 	else
-		static_assert(false);
+	{
+		static_assert(false, "Not implemented ");
+		return static_cast<TypeCode>(0);
+	}
+}
+
+constexpr size_t GetTypeSize(const TypeCode code)
+{
+	switch (code)
+	{
+		case TypeCode::u8:
+		case TypeCode::i8:
+			return sizeof(uint8_t);
+
+		case TypeCode::u16:
+		case TypeCode::i16:
+			return sizeof(uint16_t);
+
+		case TypeCode::u32:
+		case TypeCode::i32:
+			return sizeof(uint32_t);
+
+		case TypeCode::u64:
+		case TypeCode::i64:
+			return sizeof(uint64_t);
+
+		case TypeCode::f32:
+			return sizeof(float);
+
+		case TypeCode::f64:
+			return sizeof(double);
+
+		case TypeCode::str:
+			return sizeof(str_t);
+
+		default:
+			ASSERT(false);
+			return 0;
+	}
 }
 
 inline const char* GetTypeName(const TypeCode code)
 {
 	return TypeCodeNames[static_cast<int>(code)];
 }
-
